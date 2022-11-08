022985c8798b5b5eb46423c38b4c1e2bd4d9a71d
Joao Lucas <jlucas2088@gmail.com>
2022-11-07 23:04:27 -0300

    first commit

---
+++ js/main2.js
@@ -0,0 +1,30 @@
+const orderNumber = count => {
+  let numberLooping = () => {
+    return count++
+  }
+  return numberLooping
+}
+const contador = orderNumber(1)
+
+class Alunos {
+  nome = ''
+  #id = 0
+  endereco = ''
+  data_nascimento = Date(Date.now())
+  email = ''
+  telefone = ''
+  avalicoes = []
+
+  constructor(name) {
+    this.nome = name
+    this.#id = contador()
+  }
+}
+
+const aluno1 = new Alunos('Kayk')
+const aluno2 = new Alunos('Elis')
+const aluno3 = new Alunos('Joao')
+
+console.log(aluno1)
+console.log(aluno2)
+console.log(aluno3)
